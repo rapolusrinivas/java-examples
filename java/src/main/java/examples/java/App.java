3e87751443fbac2d5e1fd536f9e4f8fb55af669d
Ashwini Rapolu <rapolu.srinivas@gmail.com>
2017-06-09 00:07:33 -0700

    pom

---
+++ java/src/main/java/examples/java/App.java
@@ -0,0 +1,13 @@
+package examples.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
